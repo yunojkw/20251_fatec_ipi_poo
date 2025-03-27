d9d1e6b6d98a6f89410934bd631a94d4bd1614bd
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-27 17:19:33 -0300

    feat(ap006-oo-p2): encerra a apostila

--- oo_parte2/JogoV2.java
+++ oo_parte2/JogoV2.java
@@ -1,27 +1,46 @@
 package oo_parte2;
 import java.util.Random;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+
 public class JogoV2 {
     public static void main(String[] args) throws Exception {
+        Class<?> classe = Personagem.class;
+        Method [] metodos = classe.getDeclaredMethods();
+        int numeroDeMetodos = 0;
+
+        for (Method metodo : metodos) {
+            if (Modifier.isPublic(metodo.getModifiers()) && !metodo.getName().equals("toString")) {
+                numeroDeMetodos++;
+            }
+        }
         var gerador = new Random();
         var p = new Personagem();
-        p.nome = "Curry";
+        var p2 = new Personagem("Lebron", 10, 10, 0);
+        var p3 = new Personagem("Soneca", 4, 4, 10);
+        Personagem [] personagens = {p, p2, p3};
         while(true){
+            var quemVaiJogar = gerador.nextInt(personagens.length);
             //1: caçar
             //2: comer
-            //3: dormir   
-            var oQueFazer = 1 + gerador.nextInt(3);//[0, 3)
+            //3: dormir
+            //descobrir quantos métodos a classe Personagem possui
+            // var qtosMetodos = Personagem.getMethods().length;   
+            var oQueFazer = 1 + gerador.nextInt(numeroDeMetodos);//[0, 3)
             switch(oQueFazer){
                 case 1:
-                    p.cacar();
+                    personagens[quemVaiJogar].cacar();
                     break;
                 case 2:
-                    p.comer();
+                    personagens[quemVaiJogar].comer();
                     break;
                 case 3:
-                    p.dormir();
+                    personagens[quemVaiJogar].dormir();
                     break;
             }
-            System.out.println(p);
+            System.out.println(personagens[quemVaiJogar]);
             System.out.println("************");
             Thread.sleep(8000);
         }
--- oo_parte2/Personagem.java
+++ oo_parte2/Personagem.java
@@ -1,12 +1,50 @@
 package oo_parte2;
-
+import java.util.Random;
 public class Personagem {
-    String nome = null;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
     
-    void cacar(){
+    private String nome = null;
+    private String [] possiveisNomes = {"Steve", "Alex", "Mark"};
+    private int energia;
+    private int fome;
+    private int sono;
+
+    public Personagem(){
+        nome = obterNomeAleatorio();
+        energia = 10;
+        fome = 0;
+        sono = 0;
+    }
+    
+    private String obterNomeAleatorio(){
+        var gerador = new Random();
+        return possiveisNomes[gerador.nextInt(possiveisNomes.length)];
+    }
+    //deve ser possivel construir um Personagem com dados passados pela classe cliente
+    //se algum valor for invalido, usar aquele que é padrão para a respectiva propriedade
+    //incluir a validação de nome: somente valem nomes que tenham pelo menos 4 caracteres
+    //se essa regra for violada, atribuir um nome aleatório
+    public Personagem(String nome, int energia, int fome, int sono){
+        this.nome = 
+            nome.length() >= 4 ? 
+            nome:
+            obterNomeAleatorio();
+        this.energia = 
+            energia >= 0 && energia <= 10 ?
+            energia:
+            10;
+        if(fome >= 0 && fome <= 10)
+            this.fome = fome;
+        else
+            this.fome = 0;
+        switch(sono){
+            case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10:
+                this.sono = sono;
+            default:
+                this.sono = 0;
+        }
+    }
+    
+    public void cacar(){
         if(energia >= 2){
             System.out.println(nome + " cacando");
             energia -= 2;
@@ -18,7 +56,7 @@ public class Personagem {
         sono = Math.min(sono + 1, 10); 
     }
 
-    void comer(){
+    public void comer(){
         if(fome >= 1){
             System.out.print(nome + " comendo\n");
             // fome = Math.max(0, fome - 1);
@@ -31,7 +69,7 @@ public class Personagem {
         }
     }
 
-    void dormir(){
+    public void dormir(){
         if(sono >= 1){
             System.out.printf("%s dormindo\n", nome);
             sono--;
